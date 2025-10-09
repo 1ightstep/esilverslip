854010cc8ef3734ca683fc9582efa41c54135e2b
lightsteps <93688603+1ightstep@users.noreply.github.com>
2025-10-09 15:45:38 -0700

    fix: fixed duplicate entries on processed requests
    Refactor handleReject function to use inRoomReject flag and remove redundant code.

--- Codebase/ESGlobal/HandleReject.js
+++ Codebase/ESGlobal/HandleReject.js
@@ -2,46 +2,46 @@ function handleReject(
   teacherBundle,
   studentBundle,
   reason,
-  changeCurrStudent = true
+  inRoomReject = true,
 ) {
-  //destination teacher curr students --;
-  if (changeCurrStudent) {
+  if (inRoomReject) {
+    //destination teacher curr students --;
     accessTeacherDB().changeCurrentStudent(teacherBundle.destTeacher, -1);
-  }
+    
+    //remove student from home teacher sheet regardless
+    const homeTeacherSheet = SpreadsheetApp.openById(
+      teacherBundle.homeSheetId,
+    ).getSheetByName("Outgoing");
+    let range = homeTeacherSheet.getDataRange();
+    let values = range.getValues();
+    let rowSelect = 2;
+    while (
+      rowSelect < values.length &&
+      values[rowSelect][2] != studentBundle.email
+    ) {
+      rowSelect++;
+    }
+    rowRange = homeTeacherSheet.getRange(rowSelect + 1, 2, 1, 4);
+    rowRange.setValues([["", "", "", ""]]);
+    rowRange.setBackground(null);
 
-  //remove student from home teacher sheet regardless
-  const homeTeacherSheet = SpreadsheetApp.openById(
-    teacherBundle.homeSheetId
-  ).getSheetByName("Outgoing");
-  let range = homeTeacherSheet.getDataRange();
-  let values = range.getValues();
-  let rowSelect = 2;
-  while (
-    rowSelect < values.length &&
-    values[rowSelect][2] != studentBundle.email
-  ) {
-    rowSelect++;
+    //remove student from student db
+    accessStudentDB().setData(studentBundle.email, {
+      email: "",
+      name: "",
+      homeroom: "",
+      destination: "",
+      purpose: "",
+      status: "",
+    });
   }
-  rowRange = homeTeacherSheet.getRange(rowSelect + 1, 2, 1, 4);
-  rowRange.setValues([["", "", "", ""]]);
-  rowRange.setBackground(null);
-
-  //remove student from student db
-  accessStudentDB().setData(studentBundle.email, {
-    email: "",
-    name: "",
-    homeroom: "",
-    destination: "",
-    purpose: "",
-    status: "",
-  });
 
   //send email to student
   sendRejectEmail(
     studentBundle.email,
     studentBundle.name,
     studentBundle.destination,
-    reason
+    reason,
   );
   //handleSubmitted auto removes student from homeroom teacher
   //only acceptances will be added to homeroom teacher
